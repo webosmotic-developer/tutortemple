8d48e4e1085345858a939bc16e90d207286c17b5
webosmotic-developer <dev.webosmotic@gmail.com>
2018-06-06 18:31:29 +0530

    Node server console logs saved in text file and logs API created for read logs in browser.

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,8 @@ import {enableProdMode} from '@angular/core';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as passport from 'passport';
+import * as fs from 'fs';
+import * as moment from 'moment';
 import {join} from 'path';
 
 import fnSetRoutes from './routes';
@@ -41,21 +43,47 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-// Set API routing
-fnSetRoutes(app);
+/*----- START: Logs File Configuration -----*/
+const LOGS_DIR_PATH = join(process.cwd(), 'logs/');
+const CURRENT_DATE_FORMAT = 'YYYY_MM_DD';
+if (!fs.existsSync(LOGS_DIR_PATH)) {
+    fs.mkdirSync(LOGS_DIR_PATH);
+}
+const fnHookSTD = (std, callback, type): any => {
+    std.write = (() => {
+        return (string): any => {
+            callback(string, type);
+        };
+    })();
+};
 
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+const fnWriteLog = (s, type) => {
+    const PP_CMD_LOGS_FILE_NAME = 'PP_LOGS_' + moment().utc().format(CURRENT_DATE_FORMAT) + '.txt';
+    const access = fs.createWriteStream(LOGS_DIR_PATH + PP_CMD_LOGS_FILE_NAME, {flags: 'a+', encoding: 'utf8'});
+    const dateFormat = moment().utc().format('YYYY-MM-DDTHH:mm:ss.SSS');
+    const msg = s.replace(/\[0m/gi, '').replace(//gi, '').replace(/\[3[0-9]m/gi, '');
+    access.write(' [' + dateFormat + '] ' + '[' + type + '] ' + msg);
+};
 
-// All regular routes use the Universal engine
-app.get('*', (req, res) => {
-    res.render('index', {req});
+fnHookSTD(process.stdout, fnWriteLog, 'INFO');
+fnHookSTD(process.stderr, fnWriteLog, 'ERROR');
+app.get('/logs', function (req, res) {
+    let DATE = moment().utc().format(CURRENT_DATE_FORMAT);
+    if (req.query.date) {
+        DATE = moment(req.query.date, CURRENT_DATE_FORMAT).format(CURRENT_DATE_FORMAT);
+    }
+    if (DATE === 'Invalid date') {
+        res.status(500).send('Invalid date. Please try YYYY/MM/DD or YYYY-MM-DD');
+    }
+    const FILE_NAME = 'PP_LOGS_' + DATE + '.txt';
+    if (fs.existsSync(LOGS_DIR_PATH + FILE_NAME)) {
+        res.sendFile(LOGS_DIR_PATH + FILE_NAME);
+    } else {
+        res.status(500).send('Logs file not available for date ' + DATE + '.');
+    }
+
 });
-
-// Start up the Node server
-app.listen(PORT, () => {
-    console.log(`Node server listening on http://localhost:${PORT}`);
-});
+/*----- END: Logs File Configuration -----*/
 
 /*----- START: Node Process events for get exceptions -----*/
 process
@@ -72,5 +100,21 @@ process
         console.error('WARNING : ', warning);
     });
 /*----- END: Node Process events for get exceptions -----*/
+
+// Set API routing
+fnSetRoutes(app);
+
+// Server static files from /browser
+app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+
+// All regular routes use the Universal engine
+app.get('*', (req, res) => {
+    res.render('index', {req});
+});
+
+// Start up the Node server
+app.listen(PORT, () => {
+    console.log(`Node server listening on http://localhost:${PORT}`);
+});
 
 export {app};
